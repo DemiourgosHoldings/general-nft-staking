ad2a7503754208efae2f8ef2d01eff91426433ad
alexbolog <mbolog.alex@gmail.com>
2023-09-07 14:52:17 +0300

    added real values to coding division staking

--- tests/module_tests/coding_division_sft_staking.rs
+++ tests/module_tests/coding_division_sft_staking.rs
@@ -18,15 +18,15 @@ fn stake() {
     ];
 
     setup.set_stake_pool_type(POOL1_TOKEN_ID, StakingModuleType::CodingDivisionSfts);
-    setup.set_token_score(POOL1_TOKEN_ID, 50);
+    setup.set_token_score(POOL1_TOKEN_ID, 5);
     setup.stake(&transfers, NO_ERR_MSG);
-    setup.assert_user_score(100);
+    setup.assert_user_score(10);
 }
 
 #[test]
 fn stake_full_set() {
-    let one_item_score = 50;
-    let full_set_bonus_score = 10;
+    let one_item_score = 5;
+    let full_set_bonus_score = 25;
 
     let mut setup = ContractSetup::new(nft_staking::contract_obj);
     let mut transfers = vec![];
@@ -48,8 +48,8 @@ fn stake_full_set() {
 
 #[test]
 fn stake_full_set_and_unstake_one_removes_full_set_score() {
-    let one_item_score = 50;
-    let full_set_bonus_score = 10;
+    let one_item_score = 5;
+    let full_set_bonus_score = 25;
 
     let mut setup = ContractSetup::new(nft_staking::contract_obj);
     let mut transfers = vec![];
