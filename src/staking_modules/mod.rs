88d22d600f6012bde2a99747d7b02543f543b8aa
alexbolog <mbolog.alex@gmail.com>
2023-09-08 15:29:46 +0300

    added snakes sft staking module impl

--- src/staking_modules/mod.rs
+++ src/staking_modules/mod.rs
@@ -1,4 +1,5 @@
 pub mod coding_division_sft_staking_module;
 pub mod default;
 pub mod invalid;
+pub mod snakes_sft_staking_module;
 pub mod staking_module_type;
--- src/staking_modules/snakes_sft_staking_module.rs
+++ src/staking_modules/snakes_sft_staking_module.rs
@@ -1,12 +1,9 @@
-use multiversx_sc::types::{BigUint, ManagedAddress, TokenIdentifier};
+use multiversx_sc::types::{BigUint, ManagedAddress, ManagedVec, TokenIdentifier};
 
 use super::{default::DefaultStakingModule, staking_module_type::VestaStakingModule};
-use crate::{
-    constants::{DEB_DENOMINATION, VESTA_CODING_DIVISION_FULL_SET_MAX_NONCE},
-    types::{nonce_qty_pair::NonceQtyPair, start_unbonding_payload::StartUnbondingPayload},
-};
+use crate::types::{nonce_qty_pair::NonceQtyPair, start_unbonding_payload::StartUnbondingPayload};
 
-pub struct CodingDivisionSftStakingModule<'a, C>
+pub struct SnakesSftStakingModule<'a, C>
 where
     C: crate::storage::config::ConfigModule,
     C: crate::storage::score::ScoreStorageModule,
@@ -18,7 +15,7 @@ where
     user_address: ManagedAddress<C::Api>,
 }
 
-impl<'a, C> CodingDivisionSftStakingModule<'a, C>
+impl<'a, C> SnakesSftStakingModule<'a, C>
 where
     C: crate::storage::config::ConfigModule,
     C: crate::storage::score::ScoreStorageModule,
@@ -38,58 +35,28 @@ where
             user_address,
         }
     }
-
-    fn count_full_sets(&self) -> BigUint<C::Api> {
-        let staked_nft_nonces = self.default_impl.get_staked_nfts_data();
-
-        let mut full_sets = BigUint::from(100_000u32);
-
-        for set_nonce in 1..=VESTA_CODING_DIVISION_FULL_SET_MAX_NONCE {
-            let item = staked_nft_nonces.iter().find(|p| p.nonce == set_nonce);
-            if item.is_none() {
-                return BigUint::zero();
-            }
-            let item_quantity = item.unwrap().quantity;
-            if item_quantity < full_sets {
-                full_sets = item_quantity;
-            }
-        }
-
-        full_sets
-    }
 }
 
-impl<'a, C> VestaStakingModule<'a, C> for CodingDivisionSftStakingModule<'a, C>
+impl<'a, C> VestaStakingModule<'a, C> for SnakesSftStakingModule<'a, C>
 where
     C: crate::storage::config::ConfigModule,
     C: crate::storage::score::ScoreStorageModule,
     C: crate::storage::user_data::UserDataStorageModule,
 {
     fn get_base_user_score(&self) -> BigUint<C::Api> {
-        let default_base_score = self.default_impl.get_base_user_score();
-        let full_sets = self.count_full_sets();
-        let full_set_score = match self.sc_ref.full_set_score(&self.impl_token_id).is_empty() {
-            true => BigUint::zero(),
-            false => BigUint::from(self.sc_ref.full_set_score(&self.impl_token_id).get()),
-        };
-
-        default_base_score + &full_sets * &full_set_score
+        BigUint::zero()
     }
 
     fn get_final_user_score(&self) -> BigUint<C::Api> {
-        let base_score = self.get_base_user_score();
-        let deb_denomination = BigUint::from(DEB_DENOMINATION);
-
-        let user_deb = match self.sc_ref.user_deb(&self.user_address).is_empty() {
-            false => self.sc_ref.user_deb(&self.user_address).get(),
-            true => deb_denomination.clone(),
-        };
-
-        &base_score * &user_deb / deb_denomination
+        BigUint::zero()
     }
 
     fn add_to_storage(&self, nonce: u64, amount: BigUint<C::Api>) {
-        let mut staked_nfts = self.default_impl.get_staked_nfts_data();
+        let mut staked_nfts = self
+            .sc_ref
+            .staked_nfts(&self.impl_token_id)
+            .remove(&self.user_address)
+            .unwrap_or_else(|| ManagedVec::new());
 
         let existing_item_index = staked_nfts.iter().position(|p| p.nonce == nonce);
         let item_to_insert;
