bb8156fa191478e5e51b0a019c6e675cbf137a27
alexbolog <mbolog.alex@gmail.com>
2023-09-11 11:29:02 +0300

    refactored score update

--- src/staking_context.rs
+++ src/staking_context.rs
@@ -1,6 +1,6 @@
 use crate::{
     staking_modules::staking_module_type::{
-        StakingModuleTypeFactory, StakingModuleTypeMapping, VestaStakingModule,
+        StakingModuleType, StakingModuleTypeFactory, StakingModuleTypeMapping, VestaStakingModule,
     },
     types::start_unbonding_payload::StartUnbondingPayload,
     utils::secure_rewards,
@@ -15,9 +15,12 @@ where
     C: crate::storage::user_data::UserDataStorageModule,
 {
     sc_ref: &'a C,
+    caller: ManagedAddress<C::Api>,
     aggregated_score: BigUint<C::Api>,
     aggregated_user_score: BigUint<C::Api>,
-    caller: ManagedAddress<C::Api>,
+    secondary_aggregated_score: BigUint<C::Api>,
+    secondary_aggregated_user_score: BigUint<C::Api>,
+    staking_module_type: StakingModuleType,
     staking_module_impl: StakingModuleTypeMapping<'a, C>,
 }
 
@@ -29,19 +32,36 @@ where
 {
     pub fn new(sc_ref: &'a C, payment_token_id: &TokenIdentifier<C::Api>) -> Self {
         let caller = sc_ref.blockchain().get_caller();
+        let staking_module_type = sc_ref.stake_pool_type_configuration(payment_token_id).get();
+
         let aggregated_score = sc_ref.aggregated_staking_score().get();
         let aggregated_user_score = match sc_ref.aggregated_user_staking_score(&caller).is_empty() {
             true => BigUint::zero(),
             false => sc_ref.aggregated_user_staking_score(&caller).get(),
         };
-        let staking_module_type = sc_ref.stake_pool_type_configuration(payment_token_id).get();
+        let secondary_aggregated_score = sc_ref
+            .aggregated_secondary_staking_score(&staking_module_type)
+            .get();
+        let secondary_aggregated_user_score = match sc_ref
+            .aggregated_user_secondary_staking_score(&staking_module_type, &caller)
+            .is_empty()
+        {
+            true => BigUint::zero(),
+            false => sc_ref
+                .aggregated_user_secondary_staking_score(&staking_module_type, &caller)
+                .get(),
+        };
+
         let staking_module_impl =
             staking_module_type.get_module(sc_ref, payment_token_id.clone(), caller.clone());
         Self {
             sc_ref,
+            caller,
             aggregated_score,
             aggregated_user_score,
-            caller,
+            secondary_aggregated_score,
+            secondary_aggregated_user_score,
+            staking_module_type,
             staking_module_impl,
         }
     }
@@ -54,40 +74,21 @@ where
                 .add_to_storage(payment.token_nonce, payment.amount);
         }
 
-        let new_user_score = self.staking_module_impl.get_final_user_score();
-        let diff = &new_user_score - &self.aggregated_user_score;
-
-        self.sc_ref
-            .aggregated_user_staking_score(&self.caller)
-            .set(new_user_score);
-        self.sc_ref
-            .aggregated_staking_score()
-            .set(&self.aggregated_score + &diff);
+        self.update_primary_score();
+        self.update_secondary_score();
     }
 
     pub fn start_unbonding(&self, payload: StartUnbondingPayload<C::Api>) -> bool {
         self.secure_rewards();
-        let initial_user_score = self
-            .sc_ref
-            .aggregated_user_staking_score(&self.caller)
-            .get();
 
         let unbonding_result = self.staking_module_impl.start_unbonding(payload.clone());
         if unbonding_result {
             self.sc_ref
                 .unbonding_assets(&self.caller)
                 .insert(self.sc_ref.blockchain().get_block_timestamp(), payload);
+            self.update_primary_score();
+            self.update_secondary_score();
         }
-
-        let new_user_score = self.staking_module_impl.get_final_user_score();
-        let score_diff = &initial_user_score - &new_user_score;
-
-        self.sc_ref
-            .aggregated_user_staking_score(&self.caller)
-            .set(new_user_score);
-        self.sc_ref
-            .aggregated_staking_score()
-            .set(&self.aggregated_score - &score_diff);
 
         unbonding_result
     }
@@ -95,5 +96,34 @@ where
     fn secure_rewards(&self) {
         let primary_reward_token_id = self.sc_ref.primary_reward_token_identifier().get();
         secure_rewards(self.sc_ref, &self.caller, &primary_reward_token_id);
+    }
+
+    fn update_primary_score(&self) {
+        let new_user_score = self.staking_module_impl.get_final_user_score();
+
+        let new_aggregated_score =
+            &self.aggregated_score - &self.aggregated_user_score + &new_user_score;
+
+        self.sc_ref
+            .aggregated_user_staking_score(&self.caller)
+            .set(new_user_score);
+        self.sc_ref
+            .aggregated_staking_score()
+            .set(new_aggregated_score);
+    }
+
+    fn update_secondary_score(&self) {
+        let new_user_score = self.staking_module_impl.get_final_secondary_score();
+
+        let new_aggregated_score = &self.secondary_aggregated_score
+            - &self.secondary_aggregated_user_score
+            + &new_user_score;
+
+        self.sc_ref
+            .aggregated_user_secondary_staking_score(&self.staking_module_type, &self.caller)
+            .set(new_user_score);
+        self.sc_ref
+            .aggregated_secondary_staking_score(&self.staking_module_type)
+            .set(new_aggregated_score);
     }
 }
