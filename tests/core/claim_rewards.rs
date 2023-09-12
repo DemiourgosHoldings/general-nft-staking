df1dfce8bf49b87b5a5bbaa4fa35d5c5e85bbf84
alexbolog <mbolog.alex@gmail.com>
2023-09-12 11:36:23 +0300

    added more test cases

--- tests/core/claim_rewards.rs
+++ tests/core/claim_rewards.rs
@@ -3,7 +3,7 @@ use nft_staking::{
 };
 
 use crate::setup::{
-    constants::{NO_ERR_MSG, POOL1_TOKEN_ID, REWARD_TOKEN_ID},
+    constants::{NO_ERR_MSG, POOL1_TOKEN_ID, REWARD_TOKEN_ID, SECONDARY_REWARD_TOKEN_ID_1},
     types::new_nft_transfer,
     ContractSetup,
 };
@@ -80,4 +80,86 @@ fn not_distributed_epoch_not_claimed() {
     setup.claim_rewards(NO_ERR_MSG);
     setup.distribute_reward(100_000, NO_ERR_MSG);
     setup.assert_pending_reward(100_000);
+}
+
+#[test]
+fn mixed_rewards_claim_successful() {
+    let mut setup = ContractSetup::new(nft_staking::contract_obj);
+    let transfer = vec![new_nft_transfer(POOL1_TOKEN_ID, 1, 1)];
+
+    setup.set_stake_pool_type(POOL1_TOKEN_ID, StakingModuleType::XBunnies);
+    setup.set_token_score(StakingModuleType::All, POOL1_TOKEN_ID, 1);
+    setup.set_token_score(StakingModuleType::XBunnies, POOL1_TOKEN_ID, 1);
+    setup.register_token_as_eligible_reward_for_pool(
+        SECONDARY_REWARD_TOKEN_ID_1,
+        StakingModuleType::XBunnies,
+    );
+
+    setup.stake(&transfer, NO_ERR_MSG);
+
+    setup.distribute_reward(100_000, NO_ERR_MSG);
+    setup.distribute_secondary_reward(
+        SECONDARY_REWARD_TOKEN_ID_1,
+        POOL1_TOKEN_ID,
+        100_000,
+        NO_ERR_MSG,
+    );
+
+    setup.claim_rewards(NO_ERR_MSG);
+
+    setup.assert_user_token_balance(REWARD_TOKEN_ID, 0, 100_000);
+    setup.assert_user_token_balance(SECONDARY_REWARD_TOKEN_ID_1, 0, 100_000);
+}
+
+#[test]
+fn mixed_partial_rewards_claim_successful() {
+    let reward_amount = 100_000;
+    let aggregated_score = 10;
+    let user_score_general_reward = 1;
+    let user_score_secondary_reward = 2;
+
+    let mut setup = ContractSetup::new(nft_staking::contract_obj);
+    let transfer = vec![new_nft_transfer(POOL1_TOKEN_ID, 1, 1)];
+
+    setup.set_stake_pool_type(POOL1_TOKEN_ID, StakingModuleType::XBunnies);
+    setup.set_token_score(
+        StakingModuleType::All,
+        POOL1_TOKEN_ID,
+        user_score_general_reward,
+    );
+    setup.set_token_score(
+        StakingModuleType::XBunnies,
+        POOL1_TOKEN_ID,
+        user_score_secondary_reward,
+    );
+    setup.register_token_as_eligible_reward_for_pool(
+        SECONDARY_REWARD_TOKEN_ID_1,
+        StakingModuleType::XBunnies,
+    );
+
+    setup.stake(&transfer, NO_ERR_MSG);
+
+    setup.set_aggregated_score(StakingModuleType::All, aggregated_score);
+    setup.set_aggregated_score(StakingModuleType::XBunnies, aggregated_score);
+    setup.distribute_reward(reward_amount, NO_ERR_MSG);
+    setup.distribute_secondary_reward(
+        SECONDARY_REWARD_TOKEN_ID_1,
+        POOL1_TOKEN_ID,
+        reward_amount,
+        NO_ERR_MSG,
+    );
+
+    setup.claim_rewards(NO_ERR_MSG);
+
+    let expected_primary_token_amount =
+        reward_amount * user_score_general_reward as u64 / aggregated_score;
+    let expected_secondary_token_amount =
+        reward_amount * user_score_secondary_reward as u64 / aggregated_score;
+
+    setup.assert_user_token_balance(REWARD_TOKEN_ID, 0, expected_primary_token_amount);
+    setup.assert_user_token_balance(
+        SECONDARY_REWARD_TOKEN_ID_1,
+        0,
+        expected_secondary_token_amount,
+    );
 }
