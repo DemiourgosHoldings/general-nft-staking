78ac4cf0004fab1cb1ef05cd450d41b83c686dc8
alexbolog <mbolog.alex@gmail.com>
2023-09-14 10:29:36 +0300

    added new context fields; extracted score data fetch

--- src/staking_context.rs
+++ src/staking_context.rs
@@ -1,6 +1,7 @@
 use crate::{
     staking_modules::staking_module_type::{
-        StakingModuleType, StakingModuleTypeFactory, StakingModuleTypeMapping, VestaStakingModule,
+        self, StakingModuleType, StakingModuleTypeFactory, StakingModuleTypeMapping,
+        VestaStakingModule,
     },
     types::start_unbonding_payload::StartUnbondingPayload,
     utils::secure_rewards,
@@ -18,8 +19,10 @@ where
     caller: ManagedAddress<C::Api>,
     aggregated_general_score: BigUint<C::Api>,
     aggregated_user_score: BigUint<C::Api>,
+    aggregated_user_score_with_deb: BigUint<C::Api>,
     secondary_aggregated_general_score: BigUint<C::Api>,
     secondary_aggregated_user_score: BigUint<C::Api>,
+    secondary_aggregated_user_score_with_deb: BigUint<C::Api>,
     initial_pool_user_score: BigUint<C::Api>,
     staking_module_type: StakingModuleType,
     staking_module_impl: StakingModuleTypeMapping<'a, C>,
@@ -35,29 +38,13 @@ where
         let caller = sc_ref.blockchain().get_caller();
         let staking_module_type = sc_ref.stake_pool_type_configuration(payment_token_id).get();
 
-        let aggregated_general_score = sc_ref
-            .aggregated_staking_score(&StakingModuleType::All)
-            .get();
-        let aggregated_user_score = match sc_ref
-            .aggregated_user_staking_score(&StakingModuleType::All, &caller)
-            .is_empty()
-        {
-            true => BigUint::zero(),
-            false => sc_ref
-                .aggregated_user_staking_score(&StakingModuleType::All, &caller)
-                .get(),
-        };
-        let secondary_aggregated_general_score =
-            sc_ref.aggregated_staking_score(&staking_module_type).get();
-        let secondary_aggregated_user_score = match sc_ref
-            .aggregated_user_staking_score(&staking_module_type, &caller)
-            .is_empty()
-        {
-            true => BigUint::zero(),
-            false => sc_ref
-                .aggregated_user_staking_score(&staking_module_type, &caller)
-                .get(),
-        };
+        let (aggregated_general_score, aggregated_user_score, aggregated_user_score_with_deb) =
+            Self::get_score_data(&sc_ref, &StakingModuleType::All, &caller);
+        let (
+            secondary_aggregated_general_score,
+            secondary_aggregated_user_score,
+            secondary_aggregated_user_score_with_deb,
+        ) = Self::get_score_data(&sc_ref, &staking_module_type, &caller);
 
         let staking_module_impl =
             staking_module_type.get_module(sc_ref, payment_token_id.clone(), caller.clone());
@@ -69,8 +56,10 @@ where
             caller,
             aggregated_general_score,
             aggregated_user_score,
+            aggregated_user_score_with_deb,
             secondary_aggregated_general_score,
             secondary_aggregated_user_score,
+            secondary_aggregated_user_score_with_deb,
             initial_pool_user_score,
             staking_module_type,
             staking_module_impl,
@@ -124,8 +113,8 @@ where
         let new_aggregated_general_score =
             &self.aggregated_general_score + &new_pool_user_score - &self.initial_pool_user_score;
 
-        let new_user_score =
-            &self.aggregated_user_score + &new_pool_user_score - &self.initial_pool_user_score;
+        let new_user_score = &self.aggregated_user_score_with_deb + &new_pool_user_score
+            - &self.initial_pool_user_score;
 
         self.sc_ref
             .aggregated_user_staking_score(&StakingModuleType::All, &self.caller)
@@ -137,12 +126,12 @@ where
 
     fn update_secondary_score(&self) {
         let new_user_score = self.staking_module_impl.get_final_secondary_score();
-        if &new_user_score == &self.secondary_aggregated_user_score {
+        if &new_user_score == &self.secondary_aggregated_user_score_with_deb {
             return;
         }
 
         let new_aggregated_general_score = &self.secondary_aggregated_general_score
-            - &self.secondary_aggregated_user_score
+            - &self.secondary_aggregated_user_score_with_deb
             + &new_user_score;
 
         self.sc_ref
@@ -151,5 +140,29 @@ where
         self.sc_ref
             .aggregated_staking_score(&self.staking_module_type)
             .set(new_aggregated_general_score);
+    }
+
+    fn get_score_data(
+        sc_ref: &'a C,
+        staking_module_type: &StakingModuleType,
+        caller: &ManagedAddress<C::Api>,
+    ) -> (BigUint<C::Api>, BigUint<C::Api>, BigUint<C::Api>) {
+        let aggregated_general_score = sc_ref.aggregated_staking_score(staking_module_type).get();
+        let aggregated_user_score = match sc_ref
+            .aggregated_user_staking_score(staking_module_type, caller)
+            .is_empty()
+        {
+            true => BigUint::zero(),
+            false => sc_ref
+                .aggregated_user_staking_score(staking_module_type, caller)
+                .get(),
+        };
+        let aggregated_user_score_with_deb = aggregated_user_score.clone();
+
+        (
+            aggregated_general_score,
+            aggregated_user_score,
+            aggregated_user_score_with_deb,
+        )
     }
 }
